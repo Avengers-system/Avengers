1e4964ecf30d7878cd338513a28b27a7fabce4d3
yggdrasil20 <yggdrasil59@gmail.com>
2017-07-26 21:03:39 +0900

    2017/07/26

--- src/main/java/com/avengers/student/mypage/controller/StudentMyPageController.java
+++ src/main/java/com/avengers/student/mypage/controller/StudentMyPageController.java
@@ -1,5 +1,28 @@
 package com.avengers.student.mypage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.avengers.student.mypage.service.StudentMypageService;
+
+@Controller
 public class StudentMyPageController {
 
+	@Autowired
+	private StudentMypageService service;
+	
+	@RequestMapping("/student/mypage/studentMypage")
+	public String studentInformation(){
+		
+		
+		return "/student/mypage/studentMypage";
+	}
+	
+	@RequestMapping("/student/mypage/studentMySchedule")
+	public String studentSchedule(){
+		
+		return "/student/mypage/studentMySchedule";
+	}
+	
 }
