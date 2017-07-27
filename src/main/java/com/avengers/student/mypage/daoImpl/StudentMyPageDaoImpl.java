92b18f75171b7bddc38af4231d3132bbdd25a6e8
0hun <whdudgns2654@gmail.com>
2017-07-27 20:21:47 +0900

    Signed-off-by: 0hun <whdudgns2654@gmail.com>

--- src/main/java/com/avengers/student/mypage/daoImpl/StudentMyPageDaoImpl.java
+++ src/main/java/com/avengers/student/mypage/daoImpl/StudentMyPageDaoImpl.java
@@ -3,12 +3,10 @@ package com.avengers.student.mypage.daoImpl;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.avengers.db.dto.DeptVO;
 import com.avengers.db.dto.PerschdVO;
 import com.avengers.db.dto.StudVO;
 import com.avengers.student.mypage.dao.StudentMyPageDao;
